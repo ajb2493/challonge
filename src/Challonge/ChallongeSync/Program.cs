d7377a3ca8f582f3ecfa9903df92205bf96beb50
ajb2493 <andrew.j.bona@gmail.com>
2015-01-29 21:50:17 -0500

    Print to Console the name of all tournaments
    Using RestSharp

--- src/Challonge/ChallongeSync/Program.cs
+++ src/Challonge/ChallongeSync/Program.cs
@@ -3,42 +3,38 @@ using System.Configuration;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using RestSharp;
 
 namespace ChallongeSync
 {
     class Program
     {
-        private const string URL = "https://api.challonge.com/v1/tournaments.json";
+        private const string URL = "https://api.challonge.com";
 
         static void Main(string[] args)
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(URL);
-
-
-            // Add an Accept header for JSON format.
-            client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
-            
             var ChallongeApiKey = ConfigurationManager.AppSettings.Get("challongeAPIKey");
-            var urlParams = String.Format("?api_key={0}&subdomain=gigmasters",ChallongeApiKey);
 
-            // List data response.
-            HttpResponseMessage response = client.GetAsync(urlParams).Result;  // Blocking call!
-            if (response.IsSuccessStatusCode)
+            //Declare Rest Client
+            var client = new RestClient(URL);
+            var request = new RestRequest("/v1/tournaments.xml", Method.GET);
+            request.AddParameter("api_key", ChallongeApiKey);
+            request.AddParameter("subdomain", "gigmasters");
+
+            var response = client.Execute<List<Tournament>>(request);
+
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                // Parse the response body. Blocking!
-                Console.WriteLine(response.Content.ReadAsStringAsync());
-                var data = response.Content.ReadAsAsync<IEnumerable<Tournament>>().Result;
                 //var dataObjects = response.Content.ReadAsAsync<IEnumerable>().Result;
-                foreach (Tournament d in data)
+                foreach (Tournament d in response.Data)
                 {
-                    Console.WriteLine("{0}", d.name);
+                    Console.WriteLine("{0}", d.Name);
                 }
             }
             else
             {
-                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ErrorMessage);
             }
             Console.Read();
         }
--- src/Challonge/ChallongeSync/Tournament.cs
+++ src/Challonge/ChallongeSync/Tournament.cs
@@ -8,72 +8,72 @@ namespace ChallongeSync
 {
     public class Tournament
     {
-        public bool accept_attachments;
-        public bool allow_participant_match_reporting;
-        public bool anonymous_voting;
-        public string category;
-        public string check_in_duration;
-        public DateTime completed_at;
-        public DateTime created_at;
-        public bool created_by_api;
-        public bool credit_capped;
-        public string description;
-        public int game_id;
-        public bool group_stages_enabled;
-        public bool hide_forum;
-        public bool hide_seeds;
-        public bool hold_third_place_match;
-        public int id;
-        public int max_predictions_per_user;
-        public string name;
-        public bool notify_users_when_matches_open;
-        public bool notify_users_when_the_tournament_ends;
-        public bool open_signup;
-        public int participants_count;
-        public int prediction_method;
-        public DateTime predictions_opened_at;
+        public bool Accept_attachments { get; set; }
+        public bool Allow_participant_match_reporting { get; set; }
+        public bool Anonymous_voting { get; set; }
+        public string Category { get; set; }
+        public string Check_in_duration { get; set; }
+        public DateTime Completed_at { get; set; }
+        public DateTime Created_at { get; set; }
+        public bool Created_by_api { get; set; }
+        public bool Credit_capped { get; set; }
+        public string Description { get; set; }
+        public int Game_id { get; set; }
+        public bool Group_stages_enabled { get; set; }
+        public bool Hide_forum { get; set; }
+        public bool Hide_seeds { get; set; }
+        public bool Hold_third_place_match { get; set; }
+        public int Id { get; set; }
+        public int Max_predictions_per_user { get; set; }
+        public string Name {get; set;}
+        public bool Notify_users_when_matches_open { get; set; }
+        public bool Notify_users_when_the_tournament_ends { get; set; }
+        public bool Open_signup { get; set; }
+        public int Participants_count { get; set; }
+        public int Prediction_method { get; set; }
+        public DateTime Predictions_opened_at { get; set; }
         //bool private;
-        public decimal progress_meter;
-        public decimal pts_for_bye;
-        public decimal pts_for_game_tie;
-        public decimal pts_for_game_win;
-        public decimal pts_for_match_tie;
-        public decimal pts_for_match_win;
-        public bool quick_advance;
-        public string ranked_by;
-        public bool require_score_agreement;
-        public decimal rr_pts_for_game_tie;
-        public decimal rr_pts_for_game_win;
-        public decimal rr_pts_for_match_tie;
-        public decimal rr_pts_for_match_win;
-        public bool sequential_pairings;
-        public bool show_rounds;
-        public object signup_cap;
-        public DateTime start_at;
-        public DateTime started_at;
-        public DateTime started_checking_in_at;
-        public string state;
-        public int swiss_rounds;
-        public int teams;
+        public decimal Progress_meter { get; set; }
+        public decimal Pts_for_bye { get; set; }
+        public decimal Pts_for_game_tie { get; set; }
+        public decimal pts_for_game_win { get; set; }
+        public decimal Pts_for_match_tie { get; set; }
+        public decimal Pts_for_match_win { get; set; }
+        public bool Quick_advance { get; set; }
+        public string Ranked_by { get; set; }
+        public bool Require_score_agreement { get; set; }
+        public decimal Rr_pts_for_game_tie { get; set; }
+        public decimal Rr_pts_for_game_win { get; set; }
+        public decimal Rr_pts_for_match_tie { get; set; }
+        public decimal Rr_pts_for_match_win { get; set; }
+        public bool Sequential_pairings { get; set; }
+        public bool Show_rounds { get; set; }
+        public object Signup_cap { get; set; }
+        public DateTime Start_at { get; set; }
+        public DateTime Started_at { get; set; }
+        public DateTime Started_checking_in_at { get; set; }
+        public string State { get; set; }
+        public int Swiss_rounds { get; set; }
+        public int Teams { get; set; }
             /*"tie_breaks": [
                 "match wins vs tied",
                 "game wins",
                 "points scored"
             ],*/
-        public string tournament_type;
-        public DateTime updated_at;
-        public string url;
-        public string description_source;
-        public string subdomain;
-        public string full_challonge_url;
-        public string live_image_url;
-        public string sign_up_url;
-        public bool review_before_finalizing;
-        public bool accepting_predictions;
-        public bool participants_locked;
-        public string game_name;
-        public bool participants_swappable;
-        public bool team_convertable;
-        public bool group_stages_were_started;
+        public string Tournament_type { get; set; }
+        public DateTime Updated_at { get; set; }
+        public string Url { get; set; }
+        public string Description_source { get; set; }
+        public string Subdomain { get; set; }
+        public string Full_challonge_url { get; set; }
+        public string Live_image_url { get; set; }
+        public string Sign_up_url { get; set; }
+        public bool Review_before_finalizing { get; set; }
+        public bool Accepting_predictions { get; set; }
+        public bool Participants_locked { get; set; }
+        public string Game_name { get; set; }
+        public bool Participants_swappable { get; set; }
+        public bool Team_convertable { get; set; }
+        public bool Group_stages_were_started { get; set; }
     }
 }
